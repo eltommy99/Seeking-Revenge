92fe260a6fc5330a08b3849b2272fd5216b05265
darias98 <darias98@hotmail.com>
2016-04-02 09:53:15 -0500

    index
    nav

--- index.php
+++ index.php
@@ -3,23 +3,32 @@
 <head>
      <meta charset="utf-8">
      <title>Seeking Revenge</title>
+     <link rel="stylesheet" type="text/css" href="estilo/index.css" >
 </head>
 <body>
-  <header>
-      <img src="seekinglogo.jpg">
-  </header>
+<nav>
+  <ul>
 
-<aside>
-</aside>
+    <li><a href="#">Soporte</a></li><hr>
 
-<section>
+    <li><a href="#">Información</a></li><hr>
 
-<article class="noticias">
+    <li><a href="#">Tienda</a></li><hr>
 
-</article>
+    <li><a href="#">Comunidad</a></li><hr>
 
+    <li><a href="#">Sesión </a>
+     <ul>
 
-</section>
+       <li><a href="#">Iniciar Sesión</a></li>
+
+       <li><a href="#">Registrarse</a></li>
+
+     </ul>
+   </li><hr>
+
+  </ul>
+</nav>
 <footer>
 2016 Masacre, Inc. Todos los derechos reservados. <br>
 Politica De Privacidad | Terminos De Uso | Foro

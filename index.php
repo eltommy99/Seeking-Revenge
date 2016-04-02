eb89fa3adf7e4487837a72daf050058f27319548
dazero <307@CEFITACA.LOCAL>
2016-04-02 10:35:49 -0500

    phpop

--- index.php
+++ index.php
@@ -4,16 +4,39 @@
      <meta charset="utf-8">
      <title>Seeking Revenge</title>
      <link rel="stylesheet" type="text/css" href="estilo/estilopopups.css">
+        <link rel="stylesheet" type="text/css" href="estilo/estilos.css">
 </head>
-<body  onload="myFunction()">
 
-  <div id="myModal" class="modal">
+<body onload= "myFunction()" >
+
+
+
+<button onclick="myInicio()">INICIAR SESION</button>
+
+
+
+
+
+
+  <div id="myModal" class="popinicial">
 
 
     <div class="modal-content">
       <span class="close">×</span>
-      <p class="pop">no olvides descargar el juego el cual sera lanzado en noviembre</p>
+      <p class="pop">No olvides descargar el juego, el cual será lanzado en Noviembre.</p>
     </div>
+
+  </div>
+
+
+  <div id="Modals" class="popsesion">
+  <div class="modal-content">
+  <span class="closes">×</span>
+  <p class="pop">USUARIO</p> <input type="text" name="Usuario"><br>
+  <p class="pop">CONTRASEÑA</p> <input type="password" name="Contraseña">
+
+  </div>
+
 
   </div>
 
@@ -27,25 +50,40 @@
 
 
 
-
   <script>
   var modal = document.getElementById('myModal');
+  var modals = document.getElementById('Modals');
 
 
 
   var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("closes")[0];
+
 
   function myFunction()  {
       modal.style.display = "block";
   }
+  function myInicio() {
+        modals.style.display = "block";
+    }
+
 
   span.onclick = function() {
       modal.style.display = "none";
   }
+  span2.onclick = function() {
+      modals.style.display = "none";
+  }
+
 
   window.onclick = function(event) {
       if (event.target == modal) {
           modal.style.display = "none";
+      }
+  }
+  window.onclick = function(event) {
+      if (event.target == modals) {
+          modals.style.display = "none";
       }
   }
   </script>

d5482767ca8b151d4e929bceaf54d8189468b213
dazero <307@CEFITACA.LOCAL>
2016-04-02 09:34:07 -0500

    pop up

--- index.php
+++ index.php
@@ -3,8 +3,52 @@
 <head>
      <meta charset="utf-8">
      <title>Seeking Revenge</title>
+     <link rel="stylesheet" type="text/css" href="estilo/estilopopups.css">
 </head>
-<body>
+<body  onload="myFunction()">
+
+  <div id="myModal" class="modal">
+
+
+    <div class="modal-content">
+      <span class="close">×</span>
+      <p class="pop">no olvides descargar el juego el cual sera lanzado en noviembre</p>
+    </div>
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+  <script>
+  var modal = document.getElementById('myModal');
+
+
+
+  var span = document.getElementsByClassName("close")[0];
+
+  function myFunction()  {
+      modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+  </script>
 
 <footer>
 2016 Masacre, Inc. Todos los derechos reservados. <br>
